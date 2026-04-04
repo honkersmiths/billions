8c34c5be2292ec500876640dc0c5f0af4fcf8862
Onque <honkersmiths@gmail.com>
2026-04-04 19:46:19 -0400

    Move code to its own files, generally tidy up

---
+++ src/main.js
@@ -0,0 +1,119 @@
+import * as THREE from 'three';
+import vertexShader from './shaders/bill.vert';
+import fragmentShader from './shaders/bill.frag';
+
+let container;
+let camera, scene, renderer;
+
+init();
+
+function init() {
+
+    container = document.getElementById( 'container' );
+
+    camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 1, 10 );
+    camera.position.z = 2;
+
+    scene = new THREE.Scene();
+
+    // geometry
+    const vector = new THREE.Vector4();
+
+    // Let's call each bill $100,000
+    const instances = 26000;
+    // const instances = 180;
+
+    const offsets = [];
+    const colors = [];
+    const orientationsStart = [];
+    const orientationsEnd = [];
+
+    // Approximate bill shape
+    const positions = new Float32Array( [
+        -0.025, -0.0625,  0.025, // v0
+        0.025, -0.0625,  0.025, // v1
+        0.025,  0.0625,  0.025, // v2
+        0.025,  0.0625,  0.025, // v3
+        -0.025,  0.0625,  0.025, // v4
+        -0.025, -0.0625,  0.025  // v5
+    ] );
+
+    // instanced attributes
+    for ( let i = 0; i < instances; i ++ ) {
+
+        // offsets
+        offsets.push( Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5 );
+
+        // colors - suggestive greens (we could use alpha, which has its benefits, but going opaque for now)
+        colors.push( Math.random() * 0.1, Math.random() * 0.3 + 0.4, Math.random() * 0.1, 1.0 );
+
+        // orientation start
+        vector.set( Math.random() * 2 - 1, Math.random() * 2 - 1, Math.random() * 2 - 1, Math.random() * 2 - 1 );
+        vector.normalize();
+
+        orientationsStart.push( vector.x, vector.y, vector.z, vector.w );
+
+        // orientation end
+        vector.set( Math.random() * 2 - 1, Math.random() * 2 - 1, Math.random() * 2 - 1, Math.random() * 2 - 1 );
+        vector.normalize();
+
+        orientationsEnd.push( vector.x, vector.y, vector.z, vector.w );
+    }
+
+    const geometry = new THREE.InstancedBufferGeometry();
+    geometry.instanceCount = instances; // set so its initialized for dat.GUI, will be set in first draw otherwise
+
+    geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( positions, 3 ) );
+
+    geometry.setAttribute( 'offset', new THREE.InstancedBufferAttribute( new Float32Array( offsets ), 3 ) );
+    geometry.setAttribute( 'color', new THREE.InstancedBufferAttribute( new Float32Array( colors ), 4 ) );
+    geometry.setAttribute( 'orientationStart', new THREE.InstancedBufferAttribute( new Float32Array( orientationsStart ), 4 ) );
+    geometry.setAttribute( 'orientationEnd', new THREE.InstancedBufferAttribute( new Float32Array( orientationsEnd ), 4 ) );
+
+    // material
+    const material = new THREE.RawShaderMaterial( {
+
+        uniforms: {
+            'time': { value: 1.0 },
+            'sineTime': { value: 1.0 }
+        },
+        vertexShader: vertexShader,
+        fragmentShader: fragmentShader,
+        side: THREE.DoubleSide,
+        forceSinglePass: true,
+        transparent: true
+
+    } );
+
+    const mesh = new THREE.Mesh( geometry, material );
+    scene.add( mesh );
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setAnimationLoop( animate );
+    container.appendChild( renderer.domElement );
+
+    window.addEventListener( 'resize', onWindowResize );
+}
+
+function onWindowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+function animate() {
+
+    const time = performance.now();
+
+    const object = scene.children[ 0 ];
+
+    object.rotation.y = time * 0.0005;
+    object.material.uniforms[ 'time' ].value = time * 0.005;
+    object.material.uniforms[ 'sineTime' ].value = Math.sin( object.material.uniforms[ 'time' ].value * 0.05 );
+
+    renderer.render( scene, camera );
+}
