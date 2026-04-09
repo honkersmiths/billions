435d5b0a95a68502bfc7c8cbb5eaaa0a9469bce7
Onque <honkersmiths@gmail.com>
2026-04-09 17:39:13 -0400

    Set base in vite config

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import glsl from 'vite-plugin-glsl';
 
 export default defineConfig({
+    base: '/billions/', // Must match the GitHub repository name
     plugins: [glsl()]
 });
